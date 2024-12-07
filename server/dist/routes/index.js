eea21c3e6cb660af9e65b82ed4dbc34ea904644b
Wyatt7709 <wwoydziak.7704@gmail.com>
2024-12-07 15:53:41 -0600

    render fix 2

--- server/dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import authRoutes from './auth-routes.js';
-import apiRoutes from './api/index.js';
-const router = Router();
-router.use('/auth', authRoutes);
-router.use('/api', apiRoutes);
-export default router;
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 
 export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
@@ -14,7 +14,7 @@ export const login = async (req: Request, res: Response) => {
     }
 
     // Verify password
-    const validPassword = await bcrypt.compare(password, user.password);
+    const validPassword = await bcryptjs.compare(password, user.password);
     if (!validPassword) {
       return res.status(401).json({ message: 'Invalid password' });
     }
